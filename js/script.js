99d8f78b2e2929236580be4ec5c7b759376a180c
SlavaPachevsky <savaukr17101980@gmail.com>
2022-12-22 21:00:11 +0200

    slider popular

--- js/script.js
+++ js/script.js
@@ -10,3 +10,47 @@ if (burger && menu) {
     body.classList.toggle("_lock");
   });
 }
+
+//---------Filter dropdown
+
+const filter = document.querySelector(".filter");
+if (filter) {
+  const items = filter.querySelectorAll(".block-filter");
+
+  items.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      item.querySelector(".block-filter__dropdown").classList.toggle("_active");
+      item.querySelector(".block-filter__icon").classList.toggle("_active");
+
+      if (event.target.classList.contains("block-filter__item")) {
+        item.querySelector(".block-filter__value").textContent =
+          event.target.textContent;
+      }
+    });
+  });
+}
+
+//------------Swiper start ----------------
+const swiper = new Swiper(".popular-slider", {
+  spaceBetween: 20,
+  slidesPerView: 3,
+  // Optional parameters
+  //   direction: "vertical",
+  //   loop: true,
+
+  // If we need pagination
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
